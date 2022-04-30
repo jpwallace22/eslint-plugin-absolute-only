f67c43ca4fce227b2a41b661ead09d8f5ab56bf1
jpwallace22 <93415734+jpwallace22@users.noreply.github.com>
2022-04-30 13:28:44 -0700

    Change compile to es5

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,32 @@
-import * as fs from "fs";
-import * as path from "path";
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = __importStar(require("fs"));
+var path = __importStar(require("path"));
 function findDirWithFile(filename) {
-    let dir = path.resolve(filename);
+    var dir = path.resolve(filename);
     do {
         dir = path.dirname(dir);
     } while (!fs.existsSync(path.join(dir, filename)) && dir !== "/");
@@ -11,11 +36,11 @@ function findDirWithFile(filename) {
     return dir;
 }
 function getBaseUrl(baseDir) {
-    let url = "";
-    ["jsconfig.json", "tsconfig.json"].forEach(filename => {
-        const fpath = path.join(baseDir, filename);
+    var url = "";
+    ["jsconfig.json", "tsconfig.json"].forEach(function (filename) {
+        var fpath = path.join(baseDir, filename);
         if (fs.existsSync(fpath)) {
-            const config = JSON.parse(fs.readFileSync(fpath).toString());
+            var config = JSON.parse(fs.readFileSync(fpath).toString());
             if (config && config.compilerOptions && config.compilerOptions.baseUrl) {
                 url = config.compilerOptions.baseUrl;
             }
@@ -30,21 +55,21 @@ module.exports.rules = {
             type: "layout",
         },
         create: function (context) {
-            const baseDir = findDirWithFile("package.json");
-            const baseUrl = getBaseUrl(baseDir);
+            var baseDir = findDirWithFile("package.json");
+            var baseUrl = getBaseUrl(baseDir);
             return {
-                ImportDeclaration(node) {
-                    const source = node.source.value;
+                ImportDeclaration: function (node) {
+                    var source = node.source.value;
                     if (source.startsWith(".")) {
-                        const filename = context.getFilename();
-                        const absolutePath = path.normalize(path.join(path.dirname(filename), source));
-                        const expectedPath = path.relative(baseUrl, absolutePath);
-                        if (source !== expectedPath) {
+                        var filename = context.getFilename();
+                        var absolutePath = path.normalize(path.join(path.dirname(filename), source));
+                        var expectedPath_1 = path.relative(baseUrl, absolutePath);
+                        if (source !== expectedPath_1) {
                             context.report({
-                                node,
-                                message: `Relative imports are not allowed. Use \`${expectedPath}\` instead of \`${source}\`.`,
+                                node: node,
+                                message: "Relative imports are not allowed. Use `".concat(expectedPath_1, "` instead of `").concat(source, "`."),
                                 fix: function (fixer) {
-                                    return fixer.replaceText(node.source, `'${expectedPath}'`);
+                                    return fixer.replaceText(node.source, "'".concat(expectedPath_1, "'"));
                                 },
                             });
                         }
